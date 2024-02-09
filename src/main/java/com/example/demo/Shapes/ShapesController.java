701df4e7e685250e664ea5c59f3bf0f6f0e3e1cf
Lyubo <lubodimoff@gmail.com>
2024-02-09 16:31:46 +0200

    Shapes can be created, lines cannot be created

--- src/main/java/com/example/demo/Shapes/ShapesController.java
+++ src/main/java/com/example/demo/Shapes/ShapesController.java
@@ -1,153 +1,124 @@
 package com.example.demo.Shapes;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import java.util.Random;
-
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.*;
 
 public class ShapesController {
     @FXML
     private ComboBox<String> shapeComboBox;
 
     @FXML
-    private Canvas drawCanvas;
+    private Pane drawPane;
 
     private double startX, startY;
 
     @FXML
     private void initialize() {
-        GraphicsContext graphicsContext = drawCanvas.getGraphicsContext2D();
-        double canvasWidth = drawCanvas.getWidth();
-        double canvasHeight = drawCanvas.getHeight();
+        drawPane.setMaxSize(400, 400);
+        drawPane.setPrefSize(400, 400);
 
-        graphicsContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        BorderStroke borderStroke = new BorderStroke(
+                Color.BLACK,
+                BorderStrokeStyle.SOLID,
+                null,
+                null
+        );
 
-        graphicsContext.setStroke(Color.BLACK);
-        graphicsContext.strokeRect(0, 0, canvasWidth, canvasHeight);
+        drawPane.setBorder(new Border(borderStroke));
 
-        drawCanvas.setOnMouseClicked(event -> {
-            String selectedShape = shapeComboBox.getValue();
-            if (selectedShape != null) {
-                switch (selectedShape) {
-                    case "Square":
-                        createSquare(event.getX(), event.getY());
-                        break;
-                        case "Rectangle":
-                        createRectangle(event.getX(), event.getY());
-                        break;
-                    case "Triangle":
-                        createTriangle(event.getX(), event.getY());
-                        break;
-                    case "Circle":
-                        createCircle(event.getX(), event.getY());
-                        break;
-                    case "Oval":
-                        createOval(event.getX(), event.getY());
-                        break;
-                    case "Abstract":
-                        createRandomAbstractShapes(event.getX(), event.getY());
-                        break;
-                }
+        drawPane.setOnMousePressed(event -> {
+            if (event.isSecondaryButtonDown()) {
+                startX = event.getX();
+                startY = event.getY();
             }
         });
 
-        drawCanvas.setOnMousePressed(event -> {
-            startX = event.getX();
-            startY = event.getY();
+        drawPane.setOnMouseReleased(event -> {
+            if (event.isSecondaryButtonDown()) {
+                double endX = event.getX();
+                double endY = event.getY();
+                createLine(startX, startY, endX, endY);
+            }
         });
 
-        drawCanvas.setOnMouseReleased(event -> {
+        drawPane.setOnMouseClicked(event -> {
             String selectedShape = shapeComboBox.getValue();
             if (selectedShape != null) {
                 switch (selectedShape) {
                     case "Line":
-                        createLine(startX, startY, event.getX(), event.getY());
+
                         break;
                     case "Dot":
                         createDot(event.getX(), event.getY());
                         break;
+                    case "Square":
+                        createSquare(event.getX(), event.getY(), Color.RED);
+                        break;
+                    case "Rectangle":
+                        createRectangle(event.getX(), event.getY(), Color.BLUE);
+                        break;
+                    case "Triangle":
+                        createTriangle(event.getX(), event.getY(), Color.GREEN);
+                        break;
+                    case "Circle":
+                        createCircle(event.getX(), event.getY(), Color.ORANGE);
+                        break;
+                    case "Oval":
+                        createOval(event.getX(), event.getY(), Color.PINK);
+                        break;
                 }
-
             }
         });
-
-    }
-
-    private void createLine(double startX, double startY, double endX, double endY) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setStroke(Color.BLACK);
-        gc.strokeLine(startX, startY, endX, endY);
-    }
-
-    private void createDot(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setFill(Color.BLACK);
-
-        double dotSize = 2;
-        gc.fillOval(x - dotSize / 2, y - dotSize / 2, dotSize, dotSize);
-    }
-
-    private void createSquare(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setFill(Color.RED);
-        gc.fillRect(x, y, 30, 30);
-    }
-
-    private void createRectangle(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setFill(Color.PINK);
-        gc.fillRect(x, y, 60, 30);
-    }
-
-    private void createTriangle(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setFill(Color.GREEN);
-
-        double[] xPoints = {x, x + 30, x + 15};
-        double[] yPoints = {y + 30, y + 30, y};
-
-        gc.fillPolygon(xPoints, yPoints, 3);
-    }
-
-    private void createCircle(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setFill(Color.BLUE);
-        gc.fillRoundRect(x, y, 30, 30, 30, 30);
-    }
-
-    private void createOval(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        gc.setFill(Color.ORANGE);
-        gc.fillRoundRect(x, y, 60, 30, 60, 30);
     }
 
 
-    private void createRandomAbstractShapes(double x, double y) {
-        GraphicsContext gc = drawCanvas.getGraphicsContext2D();
-        Random random = new Random();
-
-        Color color = Color.color(random.nextDouble(), random.nextDouble(), random.nextDouble());
-        gc.setFill(color);
-
-        int minSides = 4;
-        int maxSides = 10;
-        int polygonSides = random.nextInt(maxSides - minSides + 1) + minSides;
-
-        double[] xPoints = new double[polygonSides];
-        double[] yPoints = new double[polygonSides];
-
-        for (int i = 0; i < polygonSides; i++) {
-            xPoints[i] = x + random.nextDouble() * 100;
-            yPoints[i] = y + random.nextDouble() * 100;
-        }
-
-        gc.fillPolygon(xPoints, yPoints, polygonSides);
+    private void createLine(double startX, double startY, double x, double y) {
+        Line line = new Line(startX, startY, x, y);
+        line.setStroke(Color.BLACK);
+        drawPane.getChildren().add(line);
     }
 
-    private void rotateShapes() {
+    private void createDot(double startX, double startY) {
+        Circle dot = new Circle(startX, startY, 1);
+        dot.setFill(Color.BLACK);
+        drawPane.getChildren().add(dot);
     }
 
+    private void createSquare(double x, double y, Paint fill) {
+        Rectangle square = new Rectangle(x, y, 30, 30);
+        square.setFill(fill);
+        drawPane.getChildren().add(square);
+    }
+
+    private void createRectangle(double x, double y, Paint fill) {
+        Rectangle rectangle = new Rectangle(x, y, 60, 30);
+        rectangle.setFill(fill);
+        drawPane.getChildren().add(rectangle);
+    }
+
+    private void createTriangle(double x, double y, Paint fill) {
+        Polygon triangle = new Polygon();
+        triangle.getPoints().addAll(x, y + 30, x + 30, y + 30, x + 15, y);
+        triangle.setFill(fill);
+        drawPane.getChildren().add(triangle);
+    }
+
+    private void createCircle(double x, double y, Paint fill) {
+        Circle circle = new Circle(x, y, 15);
+        circle.setFill(fill);
+        drawPane.getChildren().add(circle);
+    }
+
+    private void createOval(double x, double y, Paint fill) {
+        Ellipse ellipse = new Ellipse(x + 30, y + 15, 30, 15);
+        ellipse.setFill(fill);
+        drawPane.getChildren().add(ellipse);
+    }
 }
