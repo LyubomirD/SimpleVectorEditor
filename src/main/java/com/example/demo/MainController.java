a5eee2e944f549907f452f11efd35a4aeaa626ef
Lyubo <lubodimoff@gmail.com>
2024-04-02 14:36:38 +0300

    Abile to download pane image ti png

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
@@ -15,11 +17,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MainController {
     /**
@@ -84,6 +89,9 @@ public class MainController {
 
     @FXML
     private Button clearPaneButton;
+
+    @FXML
+    private Button download;
     //End FXML components
 
     //Start Global Variables
@@ -118,7 +126,11 @@ public class MainController {
 
         // Erase functionality
         clearPaneButton.setOnAction(this::clearTheWholePane);
+
+        // Download pane
+        download.setOnAction(this::captureAndDownloadPane);
     }
+
     //End Initialize method
 
 
@@ -369,4 +381,27 @@ public class MainController {
         }
     }
     //End clear pane
+
+
+    //Start download pane
+    private void captureAndDownloadPane(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png"));
+        File file = fileChooser.showSaveDialog(((Node) event.getSource()).getScene().getWindow());
+
+        if (file != null) {
+            savePaneAsImage(drawPane, file);
+        }
+    }
+
+    private void savePaneAsImage(Pane pane, File file) {
+        WritableImage writableImage = pane.snapshot(new SnapshotParameters(), null);
+
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //End download pane
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive java.desktop;
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
+    requires javafx.swing;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
